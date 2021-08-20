e02f38f6ecf34e93b2d7cf9ec7b3aedbe67cbba1
Andreas Karis <akaris@redhat.com>
2021-08-20 15:57:12 +0200

    plot in both formatc, compact and legacy

--- plot.sh
+++ plot.sh
@@ -3,7 +3,12 @@
 if ! [ -d output ];then 
 	mkdir output
 fi
-podman exec -it ovn /bin/bash -c "/scripts/ovnkube-plot --nb-address=tcp://localhost:9641 2>/dev/null" > output/plot.txt
-cat output/plot.txt | dot -Tpdf > output/plot.pdf
-cat output/plot.txt | dot -Tsvg > output/plot.svg
-cat output/plot.txt | dot -Tpng > output/plot.png
+podman exec -it ovn /bin/bash -c "/scripts/ovnkube-plot --nb-address=tcp://localhost:9641 2>/dev/null" > output/compact.txt
+cat output/compact.txt | dot -Tpdf > output/compact.pdf
+cat output/compact.txt | dot -Tsvg > output/compact.svg
+cat output/compact.txt | dot -Tpng > output/compact.png
+
+podman exec -it ovn /bin/bash -c "/scripts/ovnkube-plot --format=legacy --nb-address=tcp://localhost:9641 2>/dev/null" > output/legacy.txt
+cat output/legacy.txt | dot -Tpdf > output/legacy.pdf
+cat output/legacy.txt | dot -Tsvg > output/legacy.svg
+cat output/legacy.txt | dot -Tpng > output/legacy.png
