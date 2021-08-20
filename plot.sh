a3f949920b83736ec7e2f28fd1ed72dd9053e7f1
Andreas Karis <akaris@redhat.com>
2021-08-20 19:02:06 +0200

    renamed legacy to detailed

--- plot.sh
+++ plot.sh
@@ -10,7 +10,7 @@ cat output/compact.txt | dot -Tpdf > output/compact.pdf
 cat output/compact.txt | dot -Tsvg > output/compact.svg
 cat output/compact.txt | dot -Tpng > output/compact.png
 
-podman exec -it ovn /bin/bash -c "/scripts/ovnkube-plot --format=legacy --nb-address=tcp://localhost:9641 2>/dev/null" > output/legacy.txt
-cat output/legacy.txt | dot -Tpdf > output/legacy.pdf
-cat output/legacy.txt | dot -Tsvg > output/legacy.svg
-cat output/legacy.txt | dot -Tpng > output/legacy.png
+podman exec -it ovn /bin/bash -c "/scripts/ovnkube-plot --format=detailed --nb-address=tcp://localhost:9641 2>/dev/null" > output/detailed.txt
+cat output/detailed.txt | dot -Tpdf > output/detailed.pdf
+cat output/detailed.txt | dot -Tsvg > output/detailed.svg
+cat output/detailed.txt | dot -Tpng > output/detailed.png
