324634463ebf85e5ccafedc70949d13bd1c4f817
Andreas Karis <akaris@redhat.com>
2021-09-27 12:35:55 -0400

    .

--- Makefile
+++ Makefile
@@ -5,7 +5,7 @@ convertdbs:
 	/bin/bash convertdbs.sh
 
 run: convertdbs
-	podman run --rm --name ovn -d --volume /tmp/ovn-databases:/etc/ovn:Z -it ovn /bin/bash
+	podman run --rm --name ovn -d -p 39641:9641 --volume /tmp/ovn-databases:/etc/ovn:Z -it ovn /bin/bash
 
 exec:
 	podman exec -it ovn /bin/bash
@@ -15,6 +15,9 @@ plot:
 
 stop:
 	podman stop ovn
+
+remove:
+	podman rm ovn
 
 logs:
 	podman logs ovn
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 if [ -f /etc/ovn/ovnnb_db.db ]; then
-	ovsdb-server -vconsole:info -vfile:off --log-file=/var/log/ovn/ovsdb-server-nb.log --remote=punix:/var/run/ovn/ovnnb_db.sock --pidfile=/var/run/ovn/ovnnb_db.pid --unixctl=/var/run/ovn/ovnnb_db.ctl --remote=ptcp:9641:localhost /etc/ovn/ovnnb_db.db &
+	ovsdb-server -vconsole:info -vfile:off --log-file=/var/log/ovn/ovsdb-server-nb.log --remote=punix:/var/run/ovn/ovnnb_db.sock --pidfile=/var/run/ovn/ovnnb_db.pid --unixctl=/var/run/ovn/ovnnb_db.ctl --remote=ptcp:9641 /etc/ovn/ovnnb_db.db &
 fi
 
 if [ -f /etc/ovn/ovnsb_db.db ]; then
-	ovsdb-server -vconsole:info -vfile:off --log-file=/var/log/ovn/ovsdb-server-sb.log --remote=punix:/var/run/ovn/ovnsb_db.sock --pidfile=/var/run/ovn/ovnsb_db.pid --unixctl=/var/run/ovn/ovnsb_db.ctl --remote=ptcp:9642:localhost /etc/ovn/ovnsb_db.db &
+	ovsdb-server -vconsole:info -vfile:off --log-file=/var/log/ovn/ovsdb-server-sb.log --remote=punix:/var/run/ovn/ovnsb_db.sock --pidfile=/var/run/ovn/ovnsb_db.pid --unixctl=/var/run/ovn/ovnsb_db.ctl --remote=ptcp:9642 /etc/ovn/ovnsb_db.db &
 fi
 sleep infinity
